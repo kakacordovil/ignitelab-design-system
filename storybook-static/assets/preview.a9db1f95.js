0fd834142c17857730bd8d1631439b015ef6689f
Karllana Cordovil <karllanacordovil@oryon.solutions>
2022-10-14 12:35:44 +0100

    commit project

---
+++ storybook-static/assets/preview.a9db1f95.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.b7c1c37b.js";import"./iframe.76288e7d.js";import"./es.map.constructor.c0e8890e.js";import"./es.number.to-fixed.5c843443.js";import"./index.95a2857f.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.a9db1f95.js.map
